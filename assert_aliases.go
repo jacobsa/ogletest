508d3431fa78538b5bf406cf74052321808b1dd5
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-12-23 11:42:55 +1100

    Fixed up assert aliases for #13.

--- assert_aliases.go
+++ assert_aliases.go
@@ -20,57 +20,57 @@ import (
 )
 
 // AssertEq(e, a) is equivalent to AssertThat(a, oglematchers.Equals(e)).
-func AssertEq(expected, actual interface{}) ExpectationResult {
-	res := AssertThat(actual, oglematchers.Equals(expected))
+func AssertEq(expected, actual interface{}, errorParts ...interface{}) ExpectationResult {
+	res := AssertThat(actual, oglematchers.Equals(expected), errorParts...)
 	res.SetCaller(getCallerForAlias())
 	return res
 }
 
 // AssertNe(e, a) is equivalent to AssertThat(a, oglematchers.Not(oglematchers.Equals(e))).
-func AssertNe(expected, actual interface{}) ExpectationResult {
-	res := AssertThat(actual, oglematchers.Not(oglematchers.Equals(expected)))
+func AssertNe(expected, actual interface{}, errorParts ...interface{}) ExpectationResult {
+	res := AssertThat(actual, oglematchers.Not(oglematchers.Equals(expected)), errorParts...)
 	res.SetCaller(getCallerForAlias())
 	return res
 }
 
 // AssertLt(x, y) is equivalent to AssertThat(x, oglematchers.LessThan(y)).
-func AssertLt(x, y interface{}) ExpectationResult {
-	res := AssertThat(x, oglematchers.LessThan(y))
+func AssertLt(x, y interface{}, errorParts ...interface{}) ExpectationResult {
+	res := AssertThat(x, oglematchers.LessThan(y), errorParts...)
 	res.SetCaller(getCallerForAlias())
 	return res
 }
 
 // AssertLe(x, y) is equivalent to AssertThat(x, oglematchers.LessOrEqual(y)).
-func AssertLe(x, y interface{}) ExpectationResult {
-	res := AssertThat(x, oglematchers.LessOrEqual(y))
+func AssertLe(x, y interface{}, errorParts ...interface{}) ExpectationResult {
+	res := AssertThat(x, oglematchers.LessOrEqual(y), errorParts...)
 	res.SetCaller(getCallerForAlias())
 	return res
 }
 
 // AssertGt(x, y) is equivalent to AssertThat(x, oglematchers.GreaterThan(y)).
-func AssertGt(x, y interface{}) ExpectationResult {
-	res := AssertThat(x, oglematchers.GreaterThan(y))
+func AssertGt(x, y interface{}, errorParts ...interface{}) ExpectationResult {
+	res := AssertThat(x, oglematchers.GreaterThan(y), errorParts...)
 	res.SetCaller(getCallerForAlias())
 	return res
 }
 
 // AssertGe(x, y) is equivalent to AssertThat(x, oglematchers.GreaterOrEqual(y)).
-func AssertGe(x, y interface{}) ExpectationResult {
-	res := AssertThat(x, oglematchers.GreaterOrEqual(y))
+func AssertGe(x, y interface{}, errorParts ...interface{}) ExpectationResult {
+	res := AssertThat(x, oglematchers.GreaterOrEqual(y), errorParts...)
 	res.SetCaller(getCallerForAlias())
 	return res
 }
 
 // AssertTrue(b) is equivalent to AssertThat(b, oglematchers.Equals(true)).
-func AssertTrue(b interface{}) ExpectationResult {
-	res := AssertThat(b, oglematchers.Equals(true))
+func AssertTrue(b interface{}, errorParts ...interface{}) ExpectationResult {
+	res := AssertThat(b, oglematchers.Equals(true), errorParts...)
 	res.SetCaller(getCallerForAlias())
 	return res
 }
 
 // AssertFalse(b) is equivalent to AssertThat(b, oglematchers.Equals(false)).
-func AssertFalse(b interface{}) ExpectationResult {
-	res := AssertThat(b, oglematchers.Equals(false))
+func AssertFalse(b interface{}, errorParts ...interface{}) ExpectationResult {
+	res := AssertThat(b, oglematchers.Equals(false), errorParts...)
 	res.SetCaller(getCallerForAlias())
 	return res
 }
