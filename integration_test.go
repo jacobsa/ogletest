9d9cd9728c73b91e82ebb9fece653b71f711f57b
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-02-09 12:49:24 +1100

    Fixed some golden test failures with Go 1.4.1.

--- integration_test.go
+++ integration_test.go
@@ -131,8 +131,8 @@ func cleanOutput(o []byte, testPkg string) []byte {
 	o = pathRe.ReplaceAll(o, []byte("/some/path/$1"))
 
 	// Replace unstable timings in gotest fail messages.
-	timingRe1 := regexp.MustCompile(`--- FAIL: .* \(\d\.\d{2} seconds\)`)
-	o = timingRe1.ReplaceAll(o, []byte("--- FAIL: somepkg (1.23 seconds)"))
+	timingRe1 := regexp.MustCompile(`--- FAIL: .* \(\d\.\d{2}s\)`)
+	o = timingRe1.ReplaceAll(o, []byte("--- FAIL: TestSomething (1.23s)"))
 
 	timingRe2 := regexp.MustCompile(`FAIL.*somepkg\s*\d\.\d{2,}s`)
 	o = timingRe2.ReplaceAll(o, []byte("FAIL somepkg 1.234s"))
