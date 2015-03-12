bb26677e856f43805f99024ba446272f489def4e
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-03-13 09:49:11 +1100

    Implemented AddFailure.

--- failure.go
+++ failure.go
@@ -15,6 +15,11 @@
 
 package ogletest
 
+import (
+	"fmt"
+	"runtime"
+)
+
 // FailureRecord represents a single failed expectation or assertion for a
 // test. Most users don't want to interact with these directly; they are
 // generated implicitly using ExpectThat, AssertThat, ExpectLt, etc.
@@ -54,7 +59,19 @@ func AddFailureRecord(r FailureRecord) {
 // Call AddFailureRecord with a record whose file name and line number come
 // from the caller of this function, and whose error string is created by
 // calling fmt.Sprintf using the arguments to this function.
-func AddFailure(format string, a ...interface{})
+func AddFailure(format string, a ...interface{}) {
+	r := FailureRecord{
+		Error: fmt.Sprintf(format, a...),
+	}
+
+	// Get information about the call site.
+	var ok bool
+	if _, r.FileName, r.LineNumber, ok = runtime.Caller(1); !ok {
+		panic("Can't find caller")
+	}
+
+	AddFailureRecord(r)
+}
 
 // Immediately stop executing the running test, causing it to fail with the
 // failures previously recorded. Behavior is undefined if no failures have been
