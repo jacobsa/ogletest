cc5d2db2e19f67cf9ba8b06afca995300e66454d
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-06-10 09:35:21 +1000

    Fixed ExpectCall.

--- expect_call.go
+++ expect_call.go
@@ -16,44 +16,34 @@
 package ogletest
 
 import (
-	"github.com/jacobsa/oglemock"
 	"runtime"
+
+	"github.com/jacobsa/oglemock"
 )
 
-// ExpectCall expresses an expectation that the method of the given name
-// should be called on the supplied mock object. It returns a function that
-// should be called with the expected arguments, matchers for the arguments,
-// or a mix of both.
+// ExpectCall expresses an expectation that the method of the given name should
+// be called on the supplied mock object. It returns a function that should be
+// called with the expected arguments, matchers for the arguments, or a mix of
+// both.
 //
 // For example:
 //
 //     mockWriter := [...]
-//     ogletest.ExpectCall(mockWriter, "Write")(oglematchers.ElementsAre(0x1))
+//     t.ExpectCall(mockWriter, "Write")(oglematchers.ElementsAre(0x1))
 //         .WillOnce(oglemock.Return(1, nil))
 //
-// This is a shortcut for calling i.MockController.ExpectCall, where i is the
-// TestInfo struct for the currently-running test. Unlike that direct approach,
-// this function automatically sets the correct file name and line number for
-// the expectation.
-func ExpectCall(o oglemock.MockObject, method string) oglemock.PartialExpecation {
+// This is a shortcut for calling t.MockController.ExpectCall. Unlike that
+// direct approach, this function automatically sets the correct file name and
+// line number for the expectation.
+func (t *T) ExpectCall(
+	o oglemock.MockObject,
+	method string) oglemock.PartialExpecation {
 	// Get information about the call site.
 	_, file, lineNumber, ok := runtime.Caller(1)
 	if !ok {
 		panic("ExpectCall: runtime.Caller")
 	}
 
-	// Grab the current test info.
-	info := currentlyRunningTest
-	if info == nil {
-		panic("ExpectCall: no test info.")
-	}
-
-	// Grab the mock controller.
-	controller := currentlyRunningTest.MockController
-	if controller == nil {
-		panic("ExpectCall: no mock controller.")
-	}
-
 	// Report the expectation.
-	return controller.ExpectCall(o, method, file, lineNumber)
+	return t.MockController.ExpectCall(o, method, file, lineNumber)
 }
