88a260e9e7ef5e5f513082e840eafb62c234f3a9
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-06-10 09:17:05 +1000

    Moved over expect aliases.

--- expect_aliases.go
+++ expect_aliases.go
@@ -18,14 +18,14 @@ package ogletest
 import "github.com/jacobsa/oglematchers"
 
 // ExpectEq(e, a) is equivalent to ExpectThat(a, oglematchers.Equals(e)).
-func ExpectEq(expected, actual interface{}, errorParts ...interface{}) {
-	expectThat(actual, oglematchers.Equals(expected), 1, errorParts)
+func (t *T) ExpectEq(expected, actual interface{}, errorParts ...interface{}) {
+	t.expectThat(actual, oglematchers.Equals(expected), 1, errorParts)
 }
 
 // ExpectNe(e, a) is equivalent to
 // ExpectThat(a, oglematchers.Not(oglematchers.Equals(e))).
-func ExpectNe(expected, actual interface{}, errorParts ...interface{}) {
-	expectThat(
+func (t *T) ExpectNe(expected, actual interface{}, errorParts ...interface{}) {
+	t.expectThat(
 		actual,
 		oglematchers.Not(oglematchers.Equals(expected)),
 		1,
@@ -33,32 +33,32 @@ func ExpectNe(expected, actual interface{}, errorParts ...interface{}) {
 }
 
 // ExpectLt(x, y) is equivalent to ExpectThat(x, oglematchers.LessThan(y)).
-func ExpectLt(x, y interface{}, errorParts ...interface{}) {
-	expectThat(x, oglematchers.LessThan(y), 1, errorParts)
+func (t *T) ExpectLt(x, y interface{}, errorParts ...interface{}) {
+	t.expectThat(x, oglematchers.LessThan(y), 1, errorParts)
 }
 
 // ExpectLe(x, y) is equivalent to ExpectThat(x, oglematchers.LessOrEqual(y)).
-func ExpectLe(x, y interface{}, errorParts ...interface{}) {
-	expectThat(x, oglematchers.LessOrEqual(y), 1, errorParts)
+func (t *T) ExpectLe(x, y interface{}, errorParts ...interface{}) {
+	t.expectThat(x, oglematchers.LessOrEqual(y), 1, errorParts)
 }
 
 // ExpectGt(x, y) is equivalent to ExpectThat(x, oglematchers.GreaterThan(y)).
-func ExpectGt(x, y interface{}, errorParts ...interface{}) {
-	expectThat(x, oglematchers.GreaterThan(y), 1, errorParts)
+func (t *T) ExpectGt(x, y interface{}, errorParts ...interface{}) {
+	t.expectThat(x, oglematchers.GreaterThan(y), 1, errorParts)
 }
 
 // ExpectGe(x, y) is equivalent to
 // ExpectThat(x, oglematchers.GreaterOrEqual(y)).
-func ExpectGe(x, y interface{}, errorParts ...interface{}) {
-	expectThat(x, oglematchers.GreaterOrEqual(y), 1, errorParts)
+func (t *T) ExpectGe(x, y interface{}, errorParts ...interface{}) {
+	t.expectThat(x, oglematchers.GreaterOrEqual(y), 1, errorParts)
 }
 
 // ExpectTrue(b) is equivalent to ExpectThat(b, oglematchers.Equals(true)).
-func ExpectTrue(b interface{}, errorParts ...interface{}) {
-	expectThat(b, oglematchers.Equals(true), 1, errorParts)
+func (t *T) ExpectTrue(b interface{}, errorParts ...interface{}) {
+	t.expectThat(b, oglematchers.Equals(true), 1, errorParts)
 }
 
 // ExpectFalse(b) is equivalent to ExpectThat(b, oglematchers.Equals(false)).
-func ExpectFalse(b interface{}, errorParts ...interface{}) {
-	expectThat(b, oglematchers.Equals(false), 1, errorParts)
+func (t *T) ExpectFalse(b interface{}, errorParts ...interface{}) {
+	t.expectThat(b, oglematchers.Equals(false), 1, errorParts)
 }
